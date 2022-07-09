15501754f7b91dc4e8150a99779944c4e89800fe
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-09 17:07:46 -0300

    add Service, Repository, Controller, Model and Dto

---
+++ src/main/java/br/com/cd2/sigabem/controllers/FreteController.java
@@ -0,0 +1,35 @@
+package br.com.cd2.sigabem.controllers;
+
+import br.com.cd2.sigabem.dto.FreteDto;
+import br.com.cd2.sigabem.models.Frete;
+import br.com.cd2.sigabem.services.FreteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/frete")
+public class FreteController {
+
+    @Autowired
+    private FreteService service;
+
+    @PostMapping
+    public ResponseEntity<Frete> insert(@RequestBody FreteDto obj){
+        Frete response = service.create(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(response.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
+    }
+    @GetMapping
+    public ResponseEntity<List<Frete>> findAll(){
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+}
---
+++ src/main/java/br/com/cd2/sigabem/repository/FreteRepository.java
@@ -0,0 +1,9 @@
+package br.com.cd2.sigabem.repository;
+
+import br.com.cd2.sigabem.models.Frete;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FreteRepository extends JpaRepository<Frete, Long> {
+}
