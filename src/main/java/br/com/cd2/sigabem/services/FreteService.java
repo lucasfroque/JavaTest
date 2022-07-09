26ebb8a1472f5e3655703820e1c1a7c68a3c36ad
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-09 19:10:55 -0300

    Implementa Calculo do frete

--- src/main/java/br/com/cd2/sigabem/services/FreteService.java
+++ src/main/java/br/com/cd2/sigabem/services/FreteService.java
@@ -3,11 +3,15 @@ package br.com.cd2.sigabem.services;
 import br.com.cd2.sigabem.dto.FreteDto;
 import br.com.cd2.sigabem.models.Frete;
 import br.com.cd2.sigabem.repository.FreteRepository;
+import br.com.cd2.sigabem.response.Cep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -16,13 +20,20 @@ public class FreteService {
     @Autowired
     private FreteRepository repository;
 
+    RestTemplate restTemplate = new RestTemplate();
+
+
     public Frete create(FreteDto freteDto){
         Frete frete = new Frete();
         frete.setCepDestino(freteDto.getCepDestino());
         frete.setCepOrigem(freteDto.getCepOrigem());
         frete.setNomeDestinatario(freteDto.getNomeDestinatario());
         frete.setPeso(freteDto.getPeso());
-        frete.setVlTotalFrete(calcFrete(freteDto.getCepDestino(), freteDto.getCepOrigem(), freteDto.getPeso()));
+        frete.setVlTotalFrete(calcValorFrete(freteDto));
+
+        frete.setDataConsulta(LocalDateTime.now());
+        frete.setDataPrevistaEntrega(LocalDateTime.now());
+
         repository.save(frete);
 
         return frete;
@@ -34,16 +45,39 @@ public class FreteService {
 
     public Frete findById(Long id) {
         Optional<Frete> obj = repository.findById(id);
-
         return obj.orElseThrow(RuntimeException::new);
     }
 
 
-    public BigDecimal calcFrete(String cepDestino, String cepOrigem, Double peso){
+    public BigDecimal calcValorFrete(FreteDto freteDto){
         Double taxPerKg = 1.0;
+        Cep cepOrigem = getInfoCep(freteDto.getCepOrigem());
+        Cep cepDestino = getInfoCep(freteDto.getCepDestino());
 
-        BigDecimal vlTotalFrete = BigDecimal.valueOf((peso * taxPerKg));
+        double vlTotalFrete = calcTax(freteDto.getPeso()) * calcDesconto(cepOrigem, cepDestino);
 
-        return vlTotalFrete;
+        return BigDecimal.valueOf(vlTotalFrete);
+    }
+
+    public Double calcTax(Double peso){
+        Double taxPerKg = 1.0;
+        return peso * taxPerKg;
+    }
+
+    public Double calcDesconto(Cep cepOrigem, Cep cepDestino){
+
+        if(Objects.equals(cepOrigem.getDdd(), cepDestino.getDdd())){
+            return  0.5;
+        }
+
+        if(Objects.equals(cepOrigem.getUf(), cepDestino.getUf())){
+            return 0.25;
+        }
+
+        return 1.0;
+    }
+
+    public Cep getInfoCep(String cep){
+        return restTemplate.getForObject("https://viacep.com.br/ws/"+cep+"/json/", Cep.class);
     }
 }
