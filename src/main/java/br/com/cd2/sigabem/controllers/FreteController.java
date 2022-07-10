41b73a6211665a836eb9cca72521725e1712b662
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-09 21:07:15 -0300

    Documenta a API

--- src/main/java/br/com/cd2/sigabem/controllers/FreteController.java
+++ src/main/java/br/com/cd2/sigabem/controllers/FreteController.java
@@ -4,6 +4,7 @@ import br.com.cd2.sigabem.dto.FreteDto;
 import br.com.cd2.sigabem.models.Frete;
 
 import br.com.cd2.sigabem.services.FreteService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class FreteController {
     @Autowired
     private FreteService service;
 
+    @Operation(summary = "Consulta e calcula o frete e salva no banco de dados")
     @PostMapping
     public ResponseEntity<Frete> insert(@RequestBody FreteDto obj){
         Frete response = service.create(obj);
@@ -28,6 +30,7 @@ public class FreteController {
 
         return ResponseEntity.created(uri).body(response);
     }
+    @Operation(summary = "Lista todos os fretes que estão salvos no banco de dados")
     @GetMapping
     public ResponseEntity<List<Frete>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
