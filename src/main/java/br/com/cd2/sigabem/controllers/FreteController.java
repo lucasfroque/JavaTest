15ebb0504eaed67e29cecdbd48d7a92832a2fd44
Lucas Fernando <lucasfernandolf38@gmail.com>
2022-07-10 23:36:14 -0300

    Atualiza documentação da API

--- src/main/java/br/com/cd2/sigabem/controllers/FreteController.java
+++ src/main/java/br/com/cd2/sigabem/controllers/FreteController.java
@@ -4,16 +4,23 @@ import br.com.cd2.sigabem.dto.FreteDto;
 import br.com.cd2.sigabem.models.Frete;
 
 import br.com.cd2.sigabem.services.FreteService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/frete")
 public class FreteController {
 
@@ -21,6 +28,11 @@ public class FreteController {
     private FreteService service;
 
     @Operation(summary = "Consulta e calcula o frete e salva no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Cep não encontrado", content = @Content(array = @ArraySchema(schema = @Schema(example = " ")))),
+            @ApiResponse(responseCode = "400", description = "Requisição incorreta", content = @Content(array = @ArraySchema(schema = @Schema(example = " ")))),
+            @ApiResponse(responseCode = "201", description = "Criado")
+    })
     @PostMapping
     public ResponseEntity<Frete> insert(@RequestBody FreteDto obj){
         Frete response = service.create(obj);
@@ -30,7 +42,9 @@ public class FreteController {
 
         return ResponseEntity.created(uri).body(response);
     }
-    @Operation(summary = "Lista todos os fretes que estão salvos no banco de dados")
+
+    @Operation(summary = "Lista todos os frete consultados")
+    @ApiResponse(responseCode = "200", description = "OK")
     @GetMapping
     public ResponseEntity<List<Frete>> findAll(){
         return ResponseEntity.ok().body(service.findAll());
